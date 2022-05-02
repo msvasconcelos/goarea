8c83f09d08359fa086d3ce26da28aea790399a47
Matheus Vasconcelos <matheus-s.v@hotmail.com>
2022-05-02 11:26:12 -0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,17 @@
+package area
+
+import "math"
+
+const Pi = 3.1416
+
+func Circ(raio float64) float64 {
+	return math.Pow(raio, 2) * Pi
+}
+
+func Rect(base, altura float64) float64 {
+	return base * altura
+}
+
+func _Triangulo(base, altura float64) float64 {
+	return (base * altura) / 2
+}
